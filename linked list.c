a9651a28890a5b71de9b3f7fcc86c55129aec37f
이예현 <shszyh@iclooud.com>
2018-12-06 15:57:11 +0900

    11

--- linked list.c
+++ linked list.c
@@ -23,3 +23,27 @@ linknd_t* create_node(int value){
 	
 	return ndPtr;
 };
+
+void addTail(int value){
+	linknd_t *ndPtr, *newPtr;
+	if(list==NULL){
+		return;
+	}	
+	else{
+		ndPtr=list;
+		while(ndPtr->next !=NULL){
+			ndPtr= ndPtr->next;
+		}
+		
+		newPtr= create_node(value);
+		ndPtr->next = newPtr;
+	}
+	
+}
+
+
+
+
+
+
+
