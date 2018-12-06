83749f3b68fd2eb7eb0d1d391fe32daceab7d726
이예현 <shszyh@iclooud.com>
2018-12-06 15:41:29 +0900

    111

--- linked list.c
+++ linked list.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct linknd{
 	int data;
@@ -9,6 +11,15 @@ static linknd_t *list;			//다른곳에서 못건들인다.
  
 linknd_t* create_node(int value){
 	linknd_t* ndPtr;
+	//동적메모리 할당  
+	ndPtr=(linknd_t*)malloc(sizeof(linknd_t)); //집이생김  
+	if (ndPtr==NULL){
+		printf("ERROR\n");
+		return NULL;
+	}
+	//정수값 저장  
+	ndPtr->data =value; //date 저장 
+	ndPtr->next =NULL; //핵심이다.  
 	
 	return ndPtr;
 };
