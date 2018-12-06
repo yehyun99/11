7f88dcc4be8367d0cfcb34944c666574ee4d367e
이예현 <shszyh@iclooud.com>
2018-12-06 15:32:44 +0900

    11

--- linked list.c
+++ linked list.c
@@ -3,6 +3,12 @@ typedef struct linknd{
 	int data;
 	void *next;
 	//struct linknd *next;
-}; linknd_t;
+} linknd_t;
 
-
+static linknd_t *list;			//다른곳에서 못건들인다. 
+ 
+linknd_t* create_node(int value){
+	linknd_t* ndPtr;
+	
+	return ndPtr;
+};
